2cc436b1d9c140cd9d92c94d8e3361b7c0feef01
JAK <james.jakthesamething@gmail.com>
2023-03-04 16:44:48 +0200

    Fixed fxml import

--- src/main/java/com/searchai/towerssearchai/Main.java
+++ src/main/java/com/searchai/towerssearchai/Main.java
@@ -3,9 +3,11 @@ package com.searchai.towerssearchai;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
 	public static void main(String[] args) {
@@ -14,11 +16,13 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("towersGui.fxml"));
 
-		Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+		StackPane stack = FXMLLoader.load(new URL(Main.class.getResource("towersGui.fxml").toExternalForm()));
+
+		Scene scene = new Scene(stack, 600, 500);
 		stage.setTitle("Towers Search Algorithms!");
 		stage.setScene(scene);
 		stage.show();
 	}
+
 }
