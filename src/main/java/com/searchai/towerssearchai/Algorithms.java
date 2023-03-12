779085f1b41b0a246547a834ba3870d9ce2161a9
PeekMe01 <ralphdaher6@gmail.com>
2023-03-12 14:45:20 +0200

    fixed B* and A* algos

--- src/main/java/com/searchai/towerssearchai/Algorithms.java
+++ src/main/java/com/searchai/towerssearchai/Algorithms.java
@@ -1,6 +1,11 @@
 package com.searchai.towerssearchai;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Algorithms {
 
@@ -10,8 +15,10 @@ public class Algorithms {
 	// 1 that it chose from first row to get the new row that it will
 	// operate on and adds that last 1 to a path list so that if it comes again to
 	// it it skips it and takes another element
-	private static final int[][] fullMap = {{0, 1, 1, 0, 0, 0, 0, 0, 0}, {1, 0, 1, 0, 0, 0, 0, 1, 0}, {1, 1, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 1, 1, 0, 0},
-			{0, 0, 0, 1, 0, 1, 0, 0, 0}, {0, 0, 1, 1, 1, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 1, 1}, {0, 1, 0, 0, 0, 0, 1, 0, 1}, {0, 0, 0, 0, 0, 0, 1, 1, 0}};
+	private static final int[][] fullMap = { { 0, 1, 1, 0, 0, 0, 0, 0, 0 }, { 1, 0, 1, 0, 0, 0, 0, 1, 0 },
+			{ 1, 1, 0, 0, 0, 1, 0, 0, 0 }, { 0, 0, 0, 0, 1, 1, 1, 0, 0 }, { 0, 0, 0, 1, 0, 1, 0, 0, 0 },
+			{ 0, 0, 1, 1, 1, 0, 0, 0, 0 }, { 0, 0, 0, 1, 0, 0, 0, 1, 1 }, { 0, 1, 0, 0, 0, 0, 1, 0, 1 },
+			{ 0, 0, 0, 0, 0, 0, 1, 1, 0 } };
 
 	/*
 	 * For thing u were saying for A*, I was doing this for best first but seems way
@@ -96,7 +103,110 @@ public class Algorithms {
 	public static ArrayList<Integer> BestFirst(int startAt, int goal) {
 
 		// From state 1 to 9 these are h functions
-		int[] heuristics = {3, 2, 3, 2, 3, 3, 1, 1, 0};
+		int[] heuristics = new int[9];// {3, 2, 3, 2, 3, 3, 1, 1, 0};
+
+		switch (goal) {
+		case 1 -> { // if goal is node 1
+			heuristics[0] = 0;
+			heuristics[1] = 1;
+			heuristics[2] = 1;
+			heuristics[3] = 3;
+			heuristics[4] = 3;
+			heuristics[5] = 2;
+			heuristics[6] = 3;
+			heuristics[7] = 2;
+			heuristics[8] = 3;
+		}
+		case 2 -> {
+			heuristics[0] = 1;
+			heuristics[1] = 0;
+			heuristics[2] = 1;
+			heuristics[3] = 3;
+			heuristics[4] = 3;
+			heuristics[5] = 2;
+			heuristics[6] = 2;
+			heuristics[7] = 1;
+			heuristics[8] = 2;
+		}
+		case 3 -> {
+			heuristics[0] = 1;
+			heuristics[1] = 1;
+			heuristics[2] = 0;
+			heuristics[3] = 2;
+			heuristics[4] = 2;
+			heuristics[5] = 1;
+			heuristics[6] = 3;
+			heuristics[7] = 2;
+			heuristics[8] = 3;
+		}
+		case 4 -> {
+			heuristics[0] = 3;
+			heuristics[1] = 3;
+			heuristics[2] = 2;
+			heuristics[3] = 0;
+			heuristics[4] = 1;
+			heuristics[5] = 1;
+			heuristics[6] = 7;
+			heuristics[7] = 2;
+			heuristics[8] = 2;
+		}
+		case 5 -> {
+			heuristics[0] = 3;
+			heuristics[1] = 3;
+			heuristics[2] = 2;
+			heuristics[3] = 1;
+			heuristics[4] = 0;
+			heuristics[5] = 1;
+			heuristics[6] = 2;
+			heuristics[7] = 3;
+			heuristics[8] = 3;
+		}
+		case 6 -> {
+			heuristics[0] = 2;
+			heuristics[1] = 2;
+			heuristics[2] = 1;
+			heuristics[3] = 1;
+			heuristics[4] = 1;
+			heuristics[5] = 0;
+			heuristics[6] = 2;
+			heuristics[7] = 3;
+			heuristics[8] = 3;
+		}
+		case 7 -> {
+			heuristics[0] = 3;
+			heuristics[1] = 2;
+			heuristics[2] = 3;
+			heuristics[3] = 1;
+			heuristics[4] = 2;
+			heuristics[5] = 2;
+			heuristics[6] = 0;
+			heuristics[7] = 1;
+			heuristics[8] = 1;
+		}
+		case 8 -> {
+			heuristics[0] = 2;
+			heuristics[1] = 1;
+			heuristics[2] = 2;
+			heuristics[3] = 2;
+			heuristics[4] = 3;
+			heuristics[5] = 3;
+			heuristics[6] = 1;
+			heuristics[7] = 0;
+			heuristics[8] = 1;
+		}
+		case 9 -> {
+			heuristics[0] = 3;
+			heuristics[1] = 2;
+			heuristics[2] = 3;
+			heuristics[3] = 2;
+			heuristics[4] = 3;
+			heuristics[5] = 3;
+			heuristics[6] = 1;
+			heuristics[7] = 1;
+			heuristics[8] = 0;
+		}
+		}
+
 		ArrayList<Integer> path = new ArrayList<>();
 		/*
 		 * In this line of code, integer is a parameter name that represents each
@@ -106,7 +216,8 @@ public class Algorithms {
 		 * corresponding heuristic value.
 		 */
 
-		PriorityQueue<Integer> stateChildren = new PriorityQueue<>(Comparator.comparingInt(integer -> heuristics[integer - 1]));
+		PriorityQueue<Integer> stateChildren = new PriorityQueue<>(
+				Comparator.comparingInt(integer -> heuristics[integer - 1]));
 
 		if (startAt == goal) {
 			path.add(startAt);
@@ -141,9 +252,216 @@ public class Algorithms {
 
 	public static ArrayList<Integer> Astar(int startAt, int goal) {
 		// From state 1 to 9 these are h functions
-		int[] heuristics = {3, 2, 3, 2, 3, 3, 1, 1, 0};
+		// these are from node 1 to goal
+		int[] heuristics = new int[9]; // {3, 2, 3, 2, 3, 3, 1, 1, 0};
+
+		switch (goal) {
+		case 1 -> { // if goal is node 1
+			heuristics[0] = 0;
+			heuristics[1] = 1;
+			heuristics[2] = 1;
+			heuristics[3] = 3;
+			heuristics[4] = 3;
+			heuristics[5] = 2;
+			heuristics[6] = 3;
+			heuristics[7] = 2;
+			heuristics[8] = 3;
+		}
+		case 2 -> {
+			heuristics[0] = 1;
+			heuristics[1] = 0;
+			heuristics[2] = 1;
+			heuristics[3] = 3;
+			heuristics[4] = 3;
+			heuristics[5] = 2;
+			heuristics[6] = 2;
+			heuristics[7] = 1;
+			heuristics[8] = 2;
+		}
+		case 3 -> {
+			heuristics[0] = 1;
+			heuristics[1] = 1;
+			heuristics[2] = 0;
+			heuristics[3] = 2;
+			heuristics[4] = 2;
+			heuristics[5] = 1;
+			heuristics[6] = 3;
+			heuristics[7] = 2;
+			heuristics[8] = 3;
+		}
+		case 4 -> {
+			heuristics[0] = 3;
+			heuristics[1] = 3;
+			heuristics[2] = 2;
+			heuristics[3] = 0;
+			heuristics[4] = 1;
+			heuristics[5] = 1;
+			heuristics[6] = 7;
+			heuristics[7] = 2;
+			heuristics[8] = 2;
+		}
+		case 5 -> {
+			heuristics[0] = 3;
+			heuristics[1] = 3;
+			heuristics[2] = 2;
+			heuristics[3] = 1;
+			heuristics[4] = 0;
+			heuristics[5] = 1;
+			heuristics[6] = 2;
+			heuristics[7] = 3;
+			heuristics[8] = 3;
+		}
+		case 6 -> {
+			heuristics[0] = 2;
+			heuristics[1] = 2;
+			heuristics[2] = 1;
+			heuristics[3] = 1;
+			heuristics[4] = 1;
+			heuristics[5] = 0;
+			heuristics[6] = 2;
+			heuristics[7] = 3;
+			heuristics[8] = 3;
+		}
+		case 7 -> {
+			heuristics[0] = 3;
+			heuristics[1] = 2;
+			heuristics[2] = 3;
+			heuristics[3] = 1;
+			heuristics[4] = 2;
+			heuristics[5] = 2;
+			heuristics[6] = 0;
+			heuristics[7] = 1;
+			heuristics[8] = 1;
+		}
+		case 8 -> {
+			heuristics[0] = 2;
+			heuristics[1] = 1;
+			heuristics[2] = 2;
+			heuristics[3] = 2;
+			heuristics[4] = 3;
+			heuristics[5] = 3;
+			heuristics[6] = 1;
+			heuristics[7] = 0;
+			heuristics[8] = 1;
+		}
+		case 9 -> {
+			heuristics[0] = 3;
+			heuristics[1] = 2;
+			heuristics[2] = 3;
+			heuristics[3] = 2;
+			heuristics[4] = 3;
+			heuristics[5] = 3;
+			heuristics[6] = 1;
+			heuristics[7] = 1;
+			heuristics[8] = 0;
+		}
+		}
+
 		// From state 1 to 9 these are the g costs for each node
-		int[] gCost = {0, 1, 1, 3, 3, 2, 3, 2, 3};
+
+		int[] gCost = new int[9];
+		switch (startAt) {
+		case 1 -> { // if starting node is 1
+			gCost[0] = 0;
+			gCost[1] = 1;
+			gCost[2] = 1;
+			gCost[3] = 3;
+			gCost[4] = 3;
+			gCost[5] = 2;
+			gCost[6] = 3;
+			gCost[7] = 2;
+			gCost[8] = 3;
+		}
+		case 2 -> {
+			gCost[0] = 1;
+			gCost[1] = 0;
+			gCost[2] = 1;
+			gCost[3] = 3;
+			gCost[4] = 3;
+			gCost[5] = 2;
+			gCost[6] = 2;
+			gCost[7] = 1;
+			gCost[8] = 2;
+		}
+		case 3 -> {
+			gCost[0] = 1;
+			gCost[1] = 1;
+			gCost[2] = 0;
+			gCost[3] = 2;
+			gCost[4] = 2;
+			gCost[5] = 1;
+			gCost[6] = 3;
+			gCost[7] = 2;
+			gCost[8] = 3;
+		}
+		case 4 -> {
+			gCost[0] = 3;
+			gCost[1] = 3;
+			gCost[2] = 2;
+			gCost[3] = 0;
+			gCost[4] = 1;
+			gCost[5] = 1;
+			gCost[6] = 7;
+			gCost[7] = 2;
+			gCost[8] = 2;
+		}
+		case 5 -> {
+			gCost[0] = 3;
+			gCost[1] = 3;
+			gCost[2] = 2;
+			gCost[3] = 1;
+			gCost[4] = 0;
+			gCost[5] = 1;
+			gCost[6] = 2;
+			gCost[7] = 3;
+			gCost[8] = 3;
+		}
+		case 6 -> {
+			gCost[0] = 2;
+			gCost[1] = 2;
+			gCost[2] = 1;
+			gCost[3] = 1;
+			gCost[4] = 1;
+			gCost[5] = 0;
+			gCost[6] = 2;
+			gCost[7] = 3;
+			gCost[8] = 3;
+		}
+		case 7 -> {
+			gCost[0] = 3;
+			gCost[1] = 2;
+			gCost[2] = 3;
+			gCost[3] = 1;
+			gCost[4] = 2;
+			gCost[5] = 2;
+			gCost[6] = 0;
+			gCost[7] = 1;
+			gCost[8] = 1;
+		}
+		case 8 -> {
+			gCost[0] = 2;
+			gCost[1] = 1;
+			gCost[2] = 2;
+			gCost[3] = 2;
+			gCost[4] = 3;
+			gCost[5] = 3;
+			gCost[6] = 1;
+			gCost[7] = 0;
+			gCost[8] = 1;
+		}
+		case 9 -> {
+			gCost[0] = 3;
+			gCost[1] = 2;
+			gCost[2] = 3;
+			gCost[3] = 2;
+			gCost[4] = 3;
+			gCost[5] = 3;
+			gCost[6] = 1;
+			gCost[7] = 1;
+			gCost[8] = 0;
+		}
+		}
+
 		int stateChildrenHead;
 
 		ArrayList<Integer> path = new ArrayList<>();
@@ -154,20 +472,31 @@ public class Algorithms {
 		} else {
 			startAt -= 1;
 		}
-		PriorityQueue<Integer> stateChildren = new PriorityQueue<>(Comparator.comparingInt(integer -> (heuristics[integer - 1]) + gCost[integer - 1]));
+
+		// System.out.println(gCost[1]+" "+gCost[2]);
+		PriorityQueue<Integer> stateChildren = new PriorityQueue<>(
+				Comparator.comparingInt(integer -> (heuristics[integer - 1] + gCost[integer - 1])));
+
 		stateChildren.add(startAt + 1);
 
 		while (!path.contains(goal)) {
+
+			System.out.println("path: " + path.toString());
+			System.out.println("state: " + stateChildren.toString() + "\n");
+
 			stateChildrenHead = stateChildren.poll();
 			if (path.contains(stateChildrenHead)) {
 				continue;
 			}
 			path.add(stateChildrenHead);
 
+			stateChildren.clear();
+
 			for (int i = 0; i < totalStates; i++) {
 				if (fullMap[stateChildrenHead - 1][i] == 1)
 					stateChildren.add(i + 1);
 			}
+
 		}
 		return path; // now return the path.
 	}
